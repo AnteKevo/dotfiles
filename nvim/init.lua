42a1ddb23a92d2d6361b93bc74c0779f704efb81
Antonio Kevo <antoniok@axis.com>
2025-02-20 08:15:13 +0100

    nvim: Disable mouse mode and change auto-complete keybinds
    Change-Id: Ibaa58a4cfbd902f772b46a415d612a2ddc95a209

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,7 +19,7 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -708,9 +708,9 @@ require("lazy").setup({
 					-- No, but seriously. Please read `:help ins-completion`, it is really good!
 					mapping = cmp.mapping.preset.insert({
 						-- Select the [n]ext item
-						["<C-n>"] = cmp.mapping.select_next_item(),
+						--["<C-n>"] = cmp.mapping.select_next_item(),
 						-- Select the [p]revious item
-						["<C-p>"] = cmp.mapping.select_prev_item(),
+						--["<C-p>"] = cmp.mapping.select_prev_item(),
 
 						-- Scroll the documentation window [b]ack / [f]orward
 						["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -719,13 +719,13 @@ require("lazy").setup({
 						-- Accept ([y]es) the completion.
 						--  This will auto-import if your LSP supports it.
 						--  This will expand snippets if the LSP sent a snippet.
-						["<C-y>"] = cmp.mapping.confirm({ select = true }),
+						--["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
 						-- If you prefer more traditional completion keymaps,
 						-- you can uncomment the following lines
-						--['<CR>'] = cmp.mapping.confirm { select = true },
-						--['<Tab>'] = cmp.mapping.select_next_item(),
-						--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						["<Tab>"] = cmp.mapping.select_next_item(),
+						["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 						-- Manually trigger a completion from nvim-cmp.
 						--  Generally you don't need this, because nvim-cmp will display
